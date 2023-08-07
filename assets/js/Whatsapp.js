768cbe7d768209b8c31b3a4340cbbec48a42a032
Juan Ondó Esono Eyanga <juanondo011099@gmail.com>
2023-08-07 17:51:51 +0100

    Panel Inicial
    Primera parte de la página web

---
+++ assets/js/Whatsapp.js
@@ -0,0 +1,14 @@
+const dameNombre = document.querySelector('#name-2');
+const dameDireccion= document.querySelector('#name-4');
+const dameTelefono= document.querySelector('#email-2');
+const dameMensaje= document.querySelector('#message-2');
+const botonEnviar= document.querySelector('#sendMessageButton');
+
+var mensaje;
+
+function enviar() {
+
+    mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%0A0A*Mensaje:*%20${dameMensaje.value}`;
+    botonEnviar.href = mensaje;
+
+}
