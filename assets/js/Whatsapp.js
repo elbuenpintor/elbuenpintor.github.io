6b05a18b5acc4a30596a49343aefe9ff40796dd2
Juan Ondó Esono Eyanga <elsaxuxesamordejuan@gmail.com>
2023-08-19 10:45:51 +0100

    campos obligatorios
    campos obligatorios en el formulario

--- assets/js/Whatsapp.js
+++ assets/js/Whatsapp.js
@@ -2,13 +2,34 @@ const dameNombre = document.querySelector('#name-2');
 const dameDireccion= document.querySelector('#name-4');
 const dameTelefono= document.querySelector('#email-2');
 const dameMensaje= document.querySelector('#message-2');
-const botonEnviar= document.querySelector('#sendMessageButton');
+const dameBoton= document.querySelector('#sendMessageButton');
 
 var mensaje;
 
-function enviar() {
-
-    mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%20%0A%0A*Mensaje:*%20${dameMensaje.value}`;
-    botonEnviar.href = mensaje;
-
-}
+dameBoton.addEventListener('click',()=>{
+    
+    if(dameNombre.value.length==0){
+        
+        alert('Por favor, complete todos los campos');
+        
+    } else if (dameDireccion.value.length==0){
+        
+        alert('Por favor, complete todos los campos');
+        
+    } else if(dameTelefono.value.length==0){
+        
+        alert('Por favor, complete todos los campos');
+        
+    } else if(dameMensaje.value.length==0){
+        
+        alert('Por favor, complete todos los campos');
+        
+    } else {
+        
+        mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%20%0A%0A*Mensaje:*%20${dameMensaje.value}`;
+       
+        dameBoton.href = mensaje;
+        
+    }
+    
+});
