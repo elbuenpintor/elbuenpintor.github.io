edaa70bbd4190d69d3af586614ecc228dadd0d8c
Juan Ondó Esono Eyanga <juanondo011099@gmail.com>
2023-09-13 14:07:57 +0100

    Parte Final
    Cambios Habilitados

--- assets/js/Whatsapp.js
+++ assets/js/Whatsapp.js
@@ -6,7 +6,15 @@ const dameBoton= document.querySelector('#sendMessageButton');
 
 var mensaje;
 
-dameBoton.addEventListener('click',()=>{
+function enviar() {
+
+    mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%20%0A%0A*Mensaje:*%20${dameMensaje.value}`;
+       
+    dameBoton.href = mensaje;
+
+}
+
+/* dameBoton.addEventListener('click',()=>{
     
     if(dameNombre.value.length==0){
         
@@ -30,6 +38,6 @@ dameBoton.addEventListener('click',()=>{
        
        dameBoton.href = mensaje;
         
-    }
+    } 
     
-});
+}); */
