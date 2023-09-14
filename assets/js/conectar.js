0bb73e71f68e7513c1e7b67884f0bc5964718830
Juan Ondó Esono Eyanga <juanondo011099@gmail.com>
2023-09-14 12:36:28 +0100

    Cambios Finales
    Todo habilitado

--- assets/js/conectar.js
+++ assets/js/conectar.js
@@ -1,14 +1,11 @@
+document.querySelector("#sendMessageButton").addEventListener('click', function(){
+
 const dameNombre = document.querySelector('#name-2');
 const dameDireccion= document.querySelector('#name-4');
 const dameTelefono= document.querySelector('#email-2');
 const dameMensaje= document.querySelector('#message-2');
-const dameBoton= document.querySelector('#sendMessageButton');
 
-var mensaje;
-
-dameBoton.addEventListener('click',()=>{
-    
-    if(dameNombre.value.length==0){
+   if(dameNombre.value.length==0){
         
         alert('Por favor, complete todos los campos');
         
@@ -24,12 +21,13 @@ dameBoton.addEventListener('click',()=>{
         
         alert('Por favor, complete todos los campos');
         
-    } else {
+    } else { 
         
-        mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%20%0A%0A*Mensaje:*%20${dameMensaje.value}`;
-       
-       dameBoton.href = mensaje;
+        let url = mensaje = `https://api.whatsapp.com/send?phone=240222392305&text=*Nombre:*%20${dameNombre.value}%0A*Direccion:*%20${dameDireccion.value}%0A*Telefono:*%20${dameTelefono.value}%20%0A%0A*Mensaje:*%20${dameMensaje.value}`;
+
+        window.open(url);
         
-    } 
+    }
     
+
 });
